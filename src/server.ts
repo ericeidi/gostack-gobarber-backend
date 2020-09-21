79ed7a93b59cd23f3074c89c1bd66d79dbab2130
Eric <ericen01@gmail.com>
2020-09-21 16:52:02 -0300

    added cors library

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction} from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -13,6 +14,7 @@ import './database';
 const app = express();
 const port = 3334;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
