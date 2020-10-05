dbe2f13194c38c0c371c036cb2f48b12f1eba1c8
Eric <ericen01@gmail.com>
2020-10-05 17:53:13 -0300

    saving tokens in the database

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -4,14 +4,21 @@ import { container } from 'tsyringe';
 import '@modules/users/providers';
 import './providers';
 
-import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
-import AppointmentsRepository from '@modules/appointments/services/infra/Typeorm/repositories/AppointmentsRepository'
+import IAppointmentsRepository from '../../modules/appointments/repositories/IAppointmentsRepository';
+import AppointmentsRepository from '../../modules/appointments/services/infra/Typeorm/repositories/AppointmentsRepository'
 
 
-import IUsersRepository from '@modules/users/repositories/IUsersRepository';
-import UsersRepository from '@modules/users/infra/Typeorm/repositories/UsersRepository'
+import IUsersRepository from '../../modules/users/repositories/IUsersRepository';
+import UsersRepository from '../../modules/users/infra/Typeorm/repositories/UsersRepository'
+
+
+ import IUserTokensRepository from '../../modules/users/repositories/IUserTokensRepository';
+ import UserTokensRepository from '../../modules/users/infra/Typeorm/repositories/UserTokensRepository'
 
 container.registerSingleton<IAppointmentsRepository>('AppointmentsRepository', AppointmentsRepository);
 
 
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
+
+
+container.registerSingleton<IUserTokensRepository>('IUserTokensRepository', UserTokensRepository);
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,12 +2,14 @@ import {Router} from 'express';
 import appointmentsRouter from '../../../../modules/appointments/services/infra/Typeorm/entities/http/routes/appointments.routes';
 import usersRouter from '../../../../modules/users/infra/Typeorm/entities/http/routes/users.routes';
 import sessionsRouter from '../../../../modules/users/infra/Typeorm/entities/http/routes/sessions.routes';
+import passwordRouter from '../../../../modules/users/infra/Typeorm/entities/http/routes/password.routes';
 
 const routes = Router();
 
 
 routes.use('/appointments', appointmentsRouter);
-routes.use('/users', usersRouter)
-routes.use('/sessions', sessionsRouter)
+routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
